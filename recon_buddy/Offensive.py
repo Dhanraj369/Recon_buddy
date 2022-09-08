9911e52e67110bd97ddfe380d8d01d3b2b8bbb8b
cipar-root <99752941+Dhanraj369@users.noreply.github.com>
2022-09-08 23:48:01 +0530

    Update Offensive.py

--- recon_buddy/Offensive.py
+++ recon_buddy/Offensive.py
@@ -184,7 +184,7 @@ def offinseve():
 
 		if get_num=="back":
 
-			os.system("python3 /usr/share/startup.py") #startup file location
+			os.system("python3 /usr/share/recon_buddy/startup.py") #startup file location
 			exit()
 
 		
