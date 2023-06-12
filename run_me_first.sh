b152bab507dfdcf344ffc3720e873785b3f45efe
Mr . Dark Sage <99752941+Dhanraj369@users.noreply.github.com>
2023-06-12 22:55:21 +0530

    Update run_me_first.sh

--- run_me_first.sh
+++ run_me_first.sh
@@ -6,55 +6,27 @@
 # github > https://github.com/Dhanraj369?tab=repositories
 
 
+if [[ $(id -u) -eq 0 ]]; then
 
-#updating and internet check
+    
+    ping -c 1 8.8.8.8 > /dev/null 2>&1
+    if [[ $? -eq 0 ]]; then
+    
+    	echo "Internet connection is online."
+	array_apllication=("scapy" "openssl" "lighttpd" "crunch " "john" "hcxpcapngtool" "reaver" "hcxdumptool" "wash"  "mdk4" "tshark" "etterlog" "hostapd" "wpaclean" "hashcat" "packetforge-ng" "asleap" "xterm" "aircrack-ng"  "pixiewps" "bully" "nft"  "aireplay-ng" "dhcpd" "airmon-ng"  "hostapd-wpe" "airodump-ng" "beef-xss" "bettercap" "lspci" "iw"  "wireshark" "airgeddon" "tcpdump" "nmap" "ettercap" "dnsmasq" "searchsploit" "wafw00f" "dmitry" "tor" "proxychains" "macchanger" "nmap" "netstat" "netdiscover" "wireshark" "dmitry" "wafw00f" "tcpdump" "searchsploit" "route" "ip" )
+    	for app in "${array_application[@]}"; do
+	    echo "Installing $app..."
+    	    sudo apt-get install -y "$app"
+    	    echo "Done installing $app."
+	done
+	
+        proxy(){
 
-check_user=`whoami`
-
-if [[ $check_user != "root" ]]; then
-	echo "enter command = sudo su"
-	sleep 5
-	exit
-
-	fi
-
-online=`fping google.com`
-if [[ $online=="google.com is alive" ]]; then
-	echo "updating ...."
-	apt-get update 
-else
-	echo "system is offline check your internet connection..";
-fi
-
-
-
-array_apllication=("scapy" "openssl" "lighttpd" "crunch " "john" "hcxpcapngtool" "reaver" "hcxdumptool" "wash"  "mdk4" "tshark" "etterlog" "hostapd" "wpaclean" "hashcat" "packetforge-ng" "asleap" "xterm" "aircrack-ng"  "pixiewps" "bully" "nft"  "aireplay-ng" "dhcpd" "airmon-ng"  "hostapd-wpe" "airodump-ng" "beef-xss" "bettercap" "lspci" "iw"  "wireshark" "airgeddon" "tcpdump" "nmap" "ettercap" "dnsmasq" "searchsploit" "wafw00f" "dmitry" "tor" "proxychains" "macchanger" "nmap" "netstat" "netdiscover" "wireshark" "dmitry" "wafw00f" "tcpdump" "searchsploit" "route" "ip" )
-
-
-for (( i = 0; i <= 53; i++ )); do
-	#apt-get install ${array_apllication[@]:$i} 
-	apt-get install ${array_apllication[$i]} -y
-
-done
-
-proxy(){
-
-    cp recon_buddy/proxychains4.conf /etc/proxychains4.conf
-}
+           cp recon_buddy/proxychains4.conf /etc/proxychains4.conf
+         }
 #malum he mere ko ak line ke liye functio ba diya jarurat nhi tha ;-)
-proxy
-
-#checking user for root
-
-pip install -r recon_buddy/requirements.txt
-
-
-
-
-check_user=`whoami`
-if [[ $check_user != "root" ]]; then
-	echo "enter command = sudo su"
-elif [[ $check_user=="root" ]]; then
+        proxy
+	pip install -r recon_buddy/requirements.txt
 	
 	echo "configering.... files..and setting up..."
 	
@@ -62,7 +34,37 @@ elif [[ $check_user=="root" ]]; then
 	cp -r recon_buddy/* /usr/share/recon_buddy
 	
 	cp recon_buddy/recon-bud  /usr/bin/
+	
+        rm -rf recon_buddy && rm run_me_first.sh
+	
+	
+    else
+    
+    	echo "Internet connection is offline."
+    fi
+       
+    
+else
+    echo "use [ sudo su ] to get root acess"
 fi
 
 
-rm -rf recon_buddy && rm run_me_first.sh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
